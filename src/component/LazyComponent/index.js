67a5e412a40b71e6bc802cd64bedd31d55917452
Mark Mu <mark.mu@totalbrain.com>
2020-04-19 00:37:58 -0700

    changed size

--- src/component/LazyComponent/index.js
+++ src/component/LazyComponent/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const VideoOrImg = () => {
     return (
-            <video style={{position: 'absolute', height: '100%'}} loop autoPlay playsInline muted>
-                <source src="./assets/videos/fengyuzhou-part.mov" type="video/mp4" />
+            <video style={{position: 'absolute', right:0, bottom:0, minHeight: '100%', minWidth: '100%'}} loop autoPlay playsInline muted>
+                <source src="./assets/videos/fengyuzhou-part.mov" type="video/mp4"/>
             </video>
             );
 }
